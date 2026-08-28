775c6eebad7a80c3fc43871a94811eb3c6ff3ea3
queenxexe <jenkins01tj@protonmail.com>
2026-08-28 19:42:45 +0100

    Added camera tests and updated test CMakeLists.txt to have access to private functions of mist to allow testing of anything within mist

--- mist/tests/test_mist.cc
+++ mist/tests/test_mist.cc
@@ -1,6 +1,40 @@
 #include <gtest/gtest.h>
 #include <Math.hpp>
 #include <physics/Physics.hpp>
+#include <components/Camera.hpp>
+#include <renderer/vulkan/VulkanHelper.hpp>
+
+void ExpectMatrixNear(const glm::mat4& a, const glm::mat4& b, float tolerance = 0.0001f) {
+	for (int column = 0; column < 4; ++column) {
+		for (int row = 0; row < 4; ++row) {
+			EXPECT_NEAR(
+				a[column][row],
+				b[column][row],
+				tolerance
+			);
+		}
+	}
+}
+
+void ExpectNearVec(const glm::vec3& a, const glm::vec3& b, float tolerance = 0.0001f) {
+	EXPECT_NEAR(a.x, b.x, tolerance);
+	EXPECT_NEAR(a.y, b.y, tolerance);
+	EXPECT_NEAR(a.z, b.z, tolerance);
+}
+
+void ExpectNearVec(const glm::vec4& a, const glm::vec4& b, float tolerance = 0.0001f) {
+	EXPECT_NEAR(a.x, b.x, tolerance);
+	EXPECT_NEAR(a.y, b.y, tolerance);
+	EXPECT_NEAR(a.z, b.z, tolerance);
+	EXPECT_NEAR(a.w, b.w, tolerance);
+}
+
+void ExpectNearQuat(const glm::quat& a, const glm::quat& b, float tolerance = 0.0001f) {
+	EXPECT_NEAR(a.x, b.x, tolerance);
+	EXPECT_NEAR(a.y, b.y, tolerance);
+	EXPECT_NEAR(a.z, b.z, tolerance);
+	EXPECT_NEAR(a.w, b.w, tolerance);
+}
 
 TEST(MistTest, collisionDetectionTest) {
 	mist::Physics physics;
@@ -235,10 +269,7 @@ TEST(MistTest, transformRotate) {
 		mist::Transform a;
 		a.Rotate(glm::radians(90.0), glm::vec3(0,1,0));
 		glm::vec3 fwd = a.Forward();
-
-		EXPECT_NEAR(fwd.x, 1.0f, 0.0001f);
-		EXPECT_NEAR(fwd.y, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.z, 0.0f, 0.0001f);
+		ExpectNearVec(fwd, glm::vec3(1,0,0));
 	}
 
 	{
@@ -267,10 +298,7 @@ TEST(MistTest, transformRotate) {
 		a.Rotate(glm::radians(90.0), glm::vec3(1,0,0));
 		a.Rotate(glm::radians(90.0), glm::vec3(1,0,0));
 		glm::vec3 fwd = a.Forward();
-
-		EXPECT_NEAR(fwd.x, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.y, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.z, -1.0f, 0.0001f);
+		ExpectNearVec(fwd, glm::vec3(0,0,-1));
 	}
 
 	{
@@ -278,10 +306,7 @@ TEST(MistTest, transformRotate) {
 		a.Rotate(glm::radians(90.0), glm::vec3(0,1,0));
 		a.Rotate(glm::radians(90.0), glm::vec3(0,1,0));
 		glm::vec3 fwd = a.Forward();
-
-		EXPECT_NEAR(fwd.x, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.y, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.z, -1.0f, 0.0001f);
+		ExpectNearVec(fwd, glm::vec3(0,0,-1));
 	}
 
 	{
@@ -289,28 +314,20 @@ TEST(MistTest, transformRotate) {
 		a.Rotate(glm::radians(90.0), glm::vec3(0,0,1));
 		a.Rotate(glm::radians(90.0), glm::vec3(0,0,1));
 		glm::vec3 fwd = a.Forward();
-
-		EXPECT_NEAR(fwd.x, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.y, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.z, 1.0f, 0.0001f);
+		ExpectNearVec(fwd, glm::vec3(0,0,1));
 	}
 }
 
 TEST(MistTest, tranformEuler) {
 	{
 		glm::quat q = mist::Transform::EulerToQuat(glm::vec3(0,0,0));
-		EXPECT_NEAR(q.x, 0.0f, 0.0001f);
-		EXPECT_NEAR(q.y, 0.0f, 0.0001f);
-		EXPECT_NEAR(q.z, 0.0f, 0.0001f);
-		EXPECT_NEAR(q.w, 1.0f, 0.0001f);
+		ExpectNearQuat(q, glm::quat(1,0,0,0));
 	}
 
 	{
 		glm::quat q = mist::Transform::EulerToQuat(glm::vec3(0,90,0));
 		glm::vec3 fwd = q * glm::vec3(0.0f, 0.0f, 1.0f);
-		EXPECT_NEAR(fwd.x, 1.0f, 0.0001f);
-		EXPECT_NEAR(fwd.y, 0.0f, 0.0001f);
-		EXPECT_NEAR(fwd.z, 0.0f, 0.0001f);
+		ExpectNearVec(fwd, glm::vec3(1,0,0));
 	}
 
 	// No gimbal lock test
@@ -318,9 +335,7 @@ TEST(MistTest, tranformEuler) {
 		glm::vec3 euler = glm::vec3(30,45,60);
 		glm::quat q = mist::Transform::EulerToQuat(euler);
 		glm::vec3 result = mist::Transform::QuatToEuler(q);
-		EXPECT_NEAR(result.x, euler.x, 0.0001f);
-		EXPECT_NEAR(result.y, euler.y, 0.0001f);
-		EXPECT_NEAR(result.z, euler.z, 0.0001f);
+		ExpectNearVec(euler, result);
 	}
 
 	// Gimbal lock test (caused by having 90)
@@ -355,9 +370,7 @@ TEST(MistTest, transformLTW) {
 		a.position = glm::vec3(10,20,30);
 		glm::mat4 matrix = a.GetLocalToWorldMatrix();
 		glm::vec4 result = matrix * glm::vec4(0,0,0,1);
-		EXPECT_NEAR(result.x, 10.0f, 0.0001f);
-		EXPECT_NEAR(result.y, 20.0f, 0.0001f);
-		EXPECT_NEAR(result.z, 30.0f, 0.0001f);
+		ExpectNearVec(result, glm::vec3(10,20,30));
 	}
 
 	{
@@ -365,9 +378,7 @@ TEST(MistTest, transformLTW) {
 		a.scale = glm::vec3(2,3,4);
 		glm::mat4 matrix = a.GetLocalToWorldMatrix();
 		glm::vec4 result = matrix * glm::vec4(1,1,1,1);
-		EXPECT_NEAR(result.x, 2.0f, 0.0001f);
-		EXPECT_NEAR(result.y, 3.0f, 0.0001f);
-		EXPECT_NEAR(result.z, 4.0f, 0.0001f);
+		ExpectNearVec(result, glm::vec3(2,3,4));
 	}
 }
 
@@ -382,11 +393,7 @@ TEST(MistTest, transformWTL) {
 		glm::mat4 wtl = a.GetWorldToLocalMatrix();
 		glm::mat4 result = ltw * wtl;
 
-		for (int row = 0; row < 4; ++row) {
-			for (int col = 0; col < 4; ++col) {
-				EXPECT_NEAR(result[row][col], glm::mat4(1.0f)[row][col], 0.0001f);
-			}
-		}
+		ExpectMatrixNear(glm::mat4(1.0f), result);
 	}
 
 	{
@@ -398,10 +405,224 @@ TEST(MistTest, transformWTL) {
 		glm::vec4 localPoint(1,2,3,4);
 		glm::vec4 worldPoint = a.GetLocalToWorldMatrix() * localPoint;
 		glm::vec4 restoredPoint = a.GetWorldToLocalMatrix() * worldPoint;
+		ExpectNearVec(restoredPoint, localPoint);
+	}
+}
 
-		EXPECT_NEAR(restoredPoint.x, localPoint.x, 0.0001f);
-		EXPECT_NEAR(restoredPoint.y, localPoint.y, 0.0001f);
-		EXPECT_NEAR(restoredPoint.z, localPoint.z, 0.0001f);
-		EXPECT_NEAR(restoredPoint.w, localPoint.w, 0.0001f);
+TEST(MistTest, cameraEquals) {
+	{
+		mist::Transform aTransform;
+		mist::Camera a(aTransform);
+		mist::Transform bTransform;
+		mist::Camera b(bTransform);
+		EXPECT_TRUE(a.IsEqual(b));
+	}
+
+	{
+		mist::Transform aTransform;
+		mist::Camera a(aTransform);
+		mist::Transform bTransform;
+		mist::Camera b(bTransform);
+		b.SetPerspectiveCamera(1920, 1080, 90, 1, 1000);
+		EXPECT_FALSE(a.IsEqual(b));
+	}
+
+	{
+		mist::Transform aTransform;
+		mist::Camera a(aTransform);
+		mist::Transform bTransform;
+		mist::Camera b(bTransform);
+		a.SetPerspectiveCamera(1920, 1080, 90, 1, 1000);
+		b.SetPerspectiveCamera(1920, 1080, 90, 1, 1000);
+		EXPECT_TRUE(a.IsEqual(b));
+	}
+
+	{
+		mist::Transform aTransform;
+		mist::Camera a(aTransform);
+		mist::Transform bTransform;
+		mist::Camera b(bTransform);
+		a.SetOrthographicCamera(1920, 1080, 10, 1, 1000);
+		b.SetPerspectiveCamera(1920, 1080, 90, 1, 1000);
+		EXPECT_FALSE(a.IsEqual(b));
+	}
+}
+
+TEST(MistTest, perspectiveCamera) {
+	const float width = 1920;
+	const float height = 1080;
+	const float near = 0.1f;
+	const float far = 1000.0f;
+	const float fov = 60.0f;
+
+	mist::Transform transform;
+	mist::Camera cam(transform);
+	cam.SetPerspectiveCamera(width, height, fov, near, far);
+	glm::mat4 expected = glm::perspectiveLH_ZO(glm::radians(fov), width / height, near, far);
+	EXPECT_EQ(cam.GetProjectionMatrix(), expected);
+}
+
+TEST(MistTest, perspectiveRatio) {
+	const float width = 1920;
+	const float height = 1080;
+	const float near = 0.1f;
+	const float far = 1000.0f;
+	const float fov = 60.0f;
+
+	mist::Transform transform;
+	mist::Camera cam(transform);
+	cam.SetPerspectiveCamera(width, height, fov, near, far);
+	glm::mat4 wide = cam.GetProjectionMatrix();
+	cam.SetPerspectiveCamera(height, width, fov, near, far);
+	glm::mat4 tall = cam.GetProjectionMatrix();
+	EXPECT_NE(wide, tall);
+}
+
+TEST(MistTest, orthographicCamera) {
+	const float width = 1920;
+	const float height = 1080;
+	const float near = -1;
+	const float far = 1;
+	const float size = 10;
+
+	mist::Transform transform;
+	mist::Camera cam(transform);
+	cam.SetOrthographicCamera(width, height, size, near, far);
+
+	float aspect = width / height;
+	float left = -size * aspect * 0.5f;
+	float right = size * aspect * 0.5f;
+	float bottom = -size * 0.5f;
+	float top = size * 0.5f;
+	glm::mat4 expected = glm::orthoLH_ZO(left, right, bottom, top, near, far);
+
+	EXPECT_EQ(cam.GetProjectionMatrix(), expected);
+}
+
+TEST(MistTest, viewportResize) {
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		cam.SetPerspectiveCamera(800, 600, 60, 0.1f, 1000);
+		glm::mat4 original = cam.GetProjectionMatrix();
+		cam.SetViewportSize(1920, 1080);
+		glm::mat4 adjusted = cam.GetProjectionMatrix();
+		EXPECT_NE(original, adjusted);
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		cam.SetOrthographicCamera(800, 600, 10, -1, 1);
+		glm::mat4 original = cam.GetProjectionMatrix();
+		cam.SetViewportSize(1920, 1080);
+		glm::mat4 adjusted = cam.GetProjectionMatrix();
+		EXPECT_NE(original, adjusted);
+	}
+}
+
+TEST(MistTest, viewMatrix) {
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		glm::mat4 expected = glm::lookAtLH(
+			glm::vec3(0,0,0),
+			glm::vec3(0,0,1),
+			glm::vec3(0,1,0)
+		);
+		EXPECT_EQ(cam.GetViewMatrix(), expected);
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		glm::mat4 original = cam.GetViewMatrix();
+		transform.position += glm::vec3(10,20,30);
+		glm::mat4 moved = cam.GetViewMatrix();
+		EXPECT_NE(original, moved);
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		glm::mat4 original = cam.GetViewMatrix();
+		transform.Rotate(glm::radians(90.0), glm::vec3(0,1,0));
+		glm::mat4 moved = cam.GetViewMatrix();
+		EXPECT_NE(original, moved);
+	}
+}
+
+TEST(MistTest, viewProjectionMatrix) {
+	mist::Transform transform;
+	mist::Camera cam(transform);
+	cam.SetPerspectiveCamera(1920, 1080, 60, 0.1f, 1000);
+	EXPECT_EQ(cam.GetViewProjectionMatrix(), cam.GetProjectionMatrix() * cam.GetViewMatrix());
+}
+
+#if DEBUG
+// Rejections are based on MIST_ASSERT which are stripped in release builds
+TEST(MistTest, cameraRejections) {
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		EXPECT_DEATH(cam.SetPerspectiveCamera(1920, 1080, 60, 100, 10), "");
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		EXPECT_DEATH(cam.SetPerspectiveCamera(0, 1080, 60, 0.1f, 1000), "");
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		EXPECT_DEATH(cam.SetPerspectiveCamera(1920, 0, 60, 0.1f, 1000), "");
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		EXPECT_DEATH(cam.SetOrthographicCamera(1920, 1080, 10, 1, -1), "");
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		EXPECT_DEATH(cam.SetOrthographicCamera(0, 1080, 10, -1, 1), "");
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		EXPECT_DEATH(cam.SetOrthographicCamera(1920, 0, 10, -1, 1), "");
+	}
+}
+#endif
+
+TEST(MistTest, vulkanProjectionFlip) {
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		cam.SetPerspectiveCamera(1920, 1080, 60, 0.1f, 1000);
+
+		glm::mat4 expected = cam.GetProjectionMatrix();
+		expected[1][1] *= -1;
+
+		glm::mat4 flipped = mist::VulkanHelper::GetFlippedProjectionMatrix(cam.GetProjectionMatrix());
+		ExpectMatrixNear(expected, flipped);
+	}
+
+	{
+		mist::Transform transform;
+		mist::Camera cam(transform);
+		cam.SetPerspectiveCamera(1920, 1080, 60, 0.1f, 1000);
+
+		glm::mat4 expected = cam.GetProjectionMatrix();
+		expected[1][1] *= -1;
+		expected *= cam.GetViewMatrix();
+
+		glm::mat4 result = mist::VulkanHelper::GetFlippedViewProjectionMatrix(cam);
+		ExpectMatrixNear(expected, result);
 	}
 }
